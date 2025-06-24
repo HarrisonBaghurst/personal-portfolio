7a6fb74111f293eb35579c6698b486ecc10710ee
Harrison Baghurst <070806harrison@gmail.com>
2025-06-24 10:42:25 +0100

    basic structure

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <section>
+      <div className="text-center p-3 bg-blue">
+        <p className="font-extrabold text-2xl text-white">
+          Website under construction
+        </p>
+      </div>
+    </section>
+  );
+}
