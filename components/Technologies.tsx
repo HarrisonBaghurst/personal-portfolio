9ba9f668d46750800ce3c37b1821f41453be792d
Harrison Baghurst <070806harrison@gmail.com>
2025-06-28 12:38:45 +0100

    updated tech movement

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,24 +1,42 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {TechnolgiesList} from '@/constants/technologies'
 import Image from 'next/image'
 
 const Technologies = () => {
-    const [scrollY, setScrollY] = useState(0);
+    const [positions, setPositions] = useState<number[]>([]);
+    const [transition, setTransition] = useState<string>('linear 0.04s');
+
+    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
         const handleScroll = () => {
-            setScrollY(window.scrollY);
+            setTransition('linear 0.04s');
+            const currentScroll = window.scrollY;
+            setPositions(Array.from({ length: TechnolgiesList.length }, (_, i) => -30 + 30 * Math.sin((i * 100 + currentScroll) * 0.0125)));
+            if(timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+            timeoutRef.current = setTimeout(() => {
+                setTransition('ease-in 0.25s');
+                setPositions(Array.from({ length: TechnolgiesList.length }, () => 0));
+            }, 150);
+        
         }
         window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            if(timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        }
     }, []);
 
     return (
         <section className='py-50 flex justify-evenly w-full px-[5%]'>
-            {TechnolgiesList.map((technology: any, index: number) => (
-                <div key={index} 
+            {TechnolgiesList.map((technology: any, i: number) => (
+                <div key={i} 
                 className='w-15 h-15
                 '>
                     <Image 
@@ -27,7 +45,8 @@ const Technologies = () => {
                     height={500}
                     alt={`${technology.name} icon`}
                     style={{
-                    transform: `translateY(${20 * Math.sin((index*50 + scrollY) * 0.015)}px) rotate(0deg)`,
+                    transform: `translateY(${positions[i]}px) rotate(0deg)`,
+                    transition: `${transition}`
                     }}
                     />
                 </div>
