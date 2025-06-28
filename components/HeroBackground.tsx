ab1a92a82c576f9b5c979d61e7e62497f15f6bf4
Harrison Baghurst <070806harrison@gmail.com>
2025-06-28 23:23:53 +0100

    optimised hero background

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -1,37 +1,43 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 
 const HeroBackgroud = () => {
-    const [scrollY, setScrollY] = useState(0);
-    const [translateY, setTranslateY] = useState(0);
-    const [rotate, setRotate] = useState(0);
-    const [zoom, setZoom] = useState(1.4); 
+    const imageRef = useRef<HTMLImageElement | null>(null);
 
     useEffect(() => {
+        let ticking = false; 
         const handleScroll = () => {
-            setScrollY(window.scrollY);
+            if(!ticking) {
+                window.requestAnimationFrame(() => {
+                    const scrollY = window.scrollY;
+                    const translateY = 0.15 * scrollY; 
+                    const rotate = -0.015 * scrollY; 
+                    const zoom = 1.4 + 0.0005 * scrollY;
+                    if(imageRef.current) {
+                        imageRef.current.style.transform = `scale(${zoom}) translateY(${translateY}px) rotate(${rotate}deg)`
+                    }   
+                    ticking = false;
+                });
+                ticking = true; 
+            }
         }
+        handleScroll();
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-    useEffect(() => {
-        setTranslateY(0.15 * scrollY);
-        setRotate(-0.015 * scrollY);
-        setZoom(1.4 + 0.0005 * scrollY);
-    }, [scrollY]);
-
     return (
         <div className='overflow-hidden h-screen'>
             <Image 
+            ref={imageRef}
             src={'/heroImage.svg'}
             alt='hero image'
             width={10000}
             height={10000}
             style={{
-                transform: `scale(${zoom}) translateY(${translateY}px) rotate(${rotate}deg)`,
+                willChange: 'transform',
             }}
             priority
             />
