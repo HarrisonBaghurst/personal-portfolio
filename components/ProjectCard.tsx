df668113a9bb74ae9eeb069a8ab321668e91bfd0
Harrison Baghurst <070806harrison@gmail.com>
2025-06-29 23:27:37 +0100

    changed project card tech text to white

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = ({title, tagline, technologies}: ProjectCardProps) => {
         <p className='responsive-p pt-3 text-grey'>
             {tagline}
         </p>
-        <div className='flex gap-5 pt-5 responsive-p text-grey'>
+        <div className='flex gap-5 pt-5 responsive-p text-white'>
             {technologies.map((technology, i) => (
                 <p key={i}>{technology}</p>
             ))}
