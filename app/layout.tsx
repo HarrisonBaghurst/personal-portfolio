25c0bfcdd12388067404cb0af02558b700c6a593
Harrison Baghurst <070806harrison@gmail.com>
2025-06-30 23:42:53 +0100

    added robots file and improved metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Harrison Baghurst",
-  description: "Personal Portfolio",
+  title: "Project Portfolio",
+  description: "Harrison Baghurst Project Portfolio",
 };
 
 export default function RootLayout({
