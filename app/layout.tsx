d02a2a201ee8e72d453f81084e87fae11c192f3d
Harrison Baghurst <070806harrison@gmail.com>
2025-07-01 10:00:26 +0100

    updated favicon and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Project Portfolio",
-  description: "Harrison Baghurst Project Portfolio",
+  title: "Harrison Baghurst - Project Portfolio",
+  description: "Showcase of Harrison Baghurst best and most recent complete and underway projects",
 };
 
 export default function RootLayout({
