546628f9f130967ec962d2aae901ecb4880b7087
Harrison Baghurst <070806harrison@gmail.com>
2025-07-05 23:26:09 +0100

    fixed card swap component

--- components/blocks/Components/CardSwap/CardSwap.tsx
+++ components/blocks/Components/CardSwap/CardSwap.tsx
@@ -40,7 +40,7 @@ export const Card = forwardRef<HTMLDivElement, CardProps>(
 );
 Card.displayName = "Card";
 
-type CardRef = RefObject<HTMLDivElement>;
+type CardRef = RefObject<HTMLDivElement | null>;
 interface Slot {
   x: number;
   y: number;
@@ -118,7 +118,7 @@ const CardSwap: React.FC<CardSwapProps> = ({
   );
 
   const tlRef = useRef<gsap.core.Timeline | null>(null);
-  const intervalRef = useRef<number>();
+  const intervalRef = useRef<number | null>(null);
   const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -200,7 +200,9 @@ const CardSwap: React.FC<CardSwapProps> = ({
       const node = container.current!;
       const pause = () => {
         tlRef.current?.pause();
-        clearInterval(intervalRef.current);
+        if(intervalRef.current !== null) {
+          clearInterval(intervalRef.current);
+        }
       };
       const resume = () => {
         tlRef.current?.play();
@@ -211,10 +213,16 @@ const CardSwap: React.FC<CardSwapProps> = ({
       return () => {
         node.removeEventListener("mouseenter", pause);
         node.removeEventListener("mouseleave", resume);
-        clearInterval(intervalRef.current);
+        if(intervalRef.current !== null) {
+          clearInterval(intervalRef.current);
+        }
       };
     }
-    return () => clearInterval(intervalRef.current);
+    return () => {
+      if(intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+      }
+    }
   }, [cardDistance, verticalDistance, delay, pauseOnHover, skewAmount, easing]);
 
   const rendered = childArr.map((child, i) =>
