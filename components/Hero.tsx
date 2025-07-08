e082615642f27738a50984f2442229ab0bee1c78
Harrison Baghurst <070806harrison@gmail.com>
2025-07-08 10:37:07 +0100

    added contact button to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react'
 import TiltedCard from './blocks/Components/TiltedCard/TiltedCard';
+import InteractButton from './InteractButton';
 
 const Hero = () => {
     const heroTextRef = useRef<HTMLDivElement | null>(null);
@@ -33,41 +34,49 @@ const Hero = () => {
             >   
                 <div 
                 ref={heroTextRef}
-                className='mt-[35dvh] flex justify-evenly'
+                className='mt-[32dvh] flex flex-col'
                 >   
-                    <div className='flex flex-col w-fit-content'>
-                        <h1 className='font-poppins font-bold text-[11rem] text-center'>Harrison Baghurst</h1>
-                        <div className='relative flex justify-evenly text-4xl pt-8'>
-                            <p>
-                                Web Developer
-                            </p>
-                            <p>
-                                •
-                            </p>
-                            <p>
-                                GCSE & A-Level Tutor
-                            </p>
-                            <p>
-                                •
-                            </p>
-                            <p>
-                                Durham Uni Undergrad
-                            </p>
+                    <div className='flex justify-evenly'>
+                        <div className='flex flex-col w-fit-content'>
+                            <h1 className='font-poppins font-bold text-[11rem] text-center'>Harrison Baghurst</h1>
+                            <div className='relative flex justify-evenly text-4xl pt-8'>
+                                <p>
+                                    Web Developer
+                                </p>
+                                <p>
+                                    •
+                                </p>
+                                <p>
+                                    GCSE & A-Level Tutor
+                                </p>
+                                <p>
+                                    •
+                                </p>
+                                <p>
+                                    Durham Uni Undergrad
+                                </p>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <TiltedCard
+                            imageSrc="https://vvz9axceq1op6mal.public.blob.vercel-storage.com/IMG-20250706-WA0005%281%29%20%281%29-PrTy2YBMtfARp5lEq2nLMsYc3Sk54b.jpg"
+                            altText="Photo of me"
+                            containerHeight="300px"
+                            containerWidth="300px"
+                            imageHeight="300px"
+                            imageWidth="300px"
+                            rotateAmplitude={15}
+                            scaleOnHover={1.1}
+                            showMobileWarning={false}
+                            showTooltip={false}
+                            displayOverlayContent={false}
+                            />
                         </div>
                     </div>
-                    <div className=''>
-                        <TiltedCard
-                        imageSrc="https://vvz9axceq1op6mal.public.blob.vercel-storage.com/IMG-20250706-WA0005%281%29%20%281%29-PrTy2YBMtfARp5lEq2nLMsYc3Sk54b.jpg"
-                        altText="Photo of me"
-                        containerHeight="300px"
-                        containerWidth="300px"
-                        imageHeight="300px"
-                        imageWidth="300px"
-                        rotateAmplitude={15}
-                        scaleOnHover={1.1}
-                        showMobileWarning={false}
-                        showTooltip={false}
-                        displayOverlayContent={false}
+                    <div className='flex justify-center mt-20'>
+                        <InteractButton
+                        text='Open Contact Form'
+                        onClick={() => {}}
                         />
                     </div>
                 </div>
