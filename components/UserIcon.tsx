403fc252cf718d310fb032acbad0064dd94cf35d
Harrison Baghurst <070806harrison@gmail.com>
2026-09-03 21:21:49 +0100

    added self photo and favicon

--- components/UserIcon.tsx
+++ components/UserIcon.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import Image from "next/image";
 
 const UserIcon = () => {
     return (
-        <div className="fixed bottom-6 right-6 size-14 sm:bottom-16 sm:right-16 sm:size-20 rounded-full bg-foreground"></div>
+        <div className="fixed overflow-hidden bottom-6 right-6 size-14 sm:bottom-16 sm:right-16 sm:size-20 rounded-full bg-foreground">
+            <Image
+                src={
+                    "https://vvz9axceq1op6mal.public.blob.vercel-storage.com/WhatsApp%20Image%202026-09-03%20at%2021.11.08.jpeg"
+                }
+                alt="Harrison Baghurst"
+                fill
+            />
+        </div>
     );
 };
 
